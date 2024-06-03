9390fbc96eb38ff90ffde9ebe4bc7b099924b227
Kelechi125 <kelechi125@yahoo.com>
2024-06-02 19:58:28 -0500

    Update the functions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const add = (num1, num2) => {
-    return num1 + num2;
+    let sum = num1 + num2;
+    console.log(sum);
 }
 
 const subtract = (num1, num2) => {
-    return num1 - num2;
+    let difference = num1 - num2;
+    console.log(difference);
 }
 
 const multiply = (num1, num2) => {
-    return num1 * num2;
+    let product = num1 * num2;
+    console.log(product);
 }
 
 const divide = (num1, num2) => {
-    return num1 / num2;
+    let quotient = num1 / num2;
+    console.log(quotient);
 }
