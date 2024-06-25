8fcad52ce7c51732dbd8d66a865a91ce108e99c7
Kelechi125 <kelechi125@yahoo.com>
2024-06-25 05:48:54 -0500

    Returned the values of the operation functions

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+// Variables for operation
 let firstNumber;
 let operator;
 let secondNumber;
 
+// Functions for different operations
+
 const add = (num1, num2) => {
     let sum = num1 + num2;
-    console.log(sum);
+    return sum;
 }
 
 const subtract = (num1, num2) => {
     let difference = num1 - num2;
-    console.log(difference);
+    return difference;
 }
 
 const multiply = (num1, num2) => {
     let product = num1 * num2;
-    console.log(product);
+    return product;
 }
 
 const divide = (num1, num2) => {
     let quotient = num1 / num2;
-    console.log(quotient);
+    return quotient;
 }
 
+// When a number is pressed, it appears in the display
+// Each consecutive number attaches to the previous number in the display (concatenation)
+// When an operator is pressed, assign the #display.textContent to firstOperand
+// Change background color of the operator
+// When a number is pressed again
+// Clear the display
+// The number appears in the display again and consecutive numbers concatenate
+
+
 const operate = (operator, firstOperand, secondOperand, callback) = {
-    
+    if
 }
